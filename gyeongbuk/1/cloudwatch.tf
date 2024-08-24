a3f44e153dcd74310e28b1faade39c0e54e514c0
clsen2024 <22a706h0659@sonline20.sen.go.kr>
2024-08-25 00:09:05 +0900

    1st test complete

--- gyeongbuk/1/cloudwatch.tf
+++ gyeongbuk/1/cloudwatch.tf
@@ -1,14 +1,20 @@
 resource "aws_cloudwatch_log_group" "customer" {
   name       = "/wsi/webapp/customer"
   kms_key_id = aws_kms_key.main.arn
+
+  depends_on = [aws_kms_key_policy.main]
 }
 
 resource "aws_cloudwatch_log_group" "product" {
   name       = "/wsi/webapp/product"
   kms_key_id = aws_kms_key.main.arn
+
+  depends_on = [aws_kms_key_policy.main]
 }
 
 resource "aws_cloudwatch_log_group" "order" {
   name       = "/wsi/webapp/order"
   kms_key_id = aws_kms_key.main.arn
+
+  depends_on = [aws_kms_key_policy.main]
 }
